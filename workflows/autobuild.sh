94bf2b121bfd1dbb98dfee4557221ea91b19bc78
HeavyDutySoul <35488618+HeavyDutySoul@users.noreply.github.com>
2024-09-09 19:44:39 +0300

    Update autobuild.sh
    Swich to HeavyDutySoul repo

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -3,7 +3,7 @@
 git submodule update --init --recursive
 sudo apt update
 sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
-git clone https://github.com/raspberrypi/pico-sdk
+git clone https://github.com/HeavyDutySoul/pico-sdk
 cd pico-sdk
 git submodule update --init
 cd ..
